6a91dbd7c5b3d8aacaba8ba736494e16f7e96c0e
Edward li <edwardtheli@gmail.com>
2025-02-25 16:30:57 +0100

    faster version no longer calls apply_rotation

--- NeuralNetwork/rotate_image.m
+++ NeuralNetwork/rotate_image.m
@@ -5,12 +5,20 @@ sz = [height(T) 3];
 typeVar = ["double","double","logical"];
 nameVar = ["X_pos", "Y_pos", "land||water"];
 rotated_image = table('Size',sz , 'VariableTypes', typeVar, 'VariableNames', nameVar);
+cosangle = cos(angle);
+sinangle = sin(angle);
+rotationVector = [cosangle,-sinangle;sinangle, cosangle];
 
-for k = 1:height(T)
-    m = apply_rotate(T{k,["Var1","Var2"]}.',angle);
-    
-    rotated_image{k,["X_pos","Y_pos"]} = m.';
-end
+X_Y_data = T{:,["Var1","Var2"]}.'
+rotated_image{:,["X_pos","Y_pos"]} = (rotationVector*X_Y_data).'
+
+%%%%%%%  slower method that called apply_rotation 
+% for k = 1:height(T)
+%     m = apply_rotate(T{k,["Var1","Var2"]}.',angle);
+% 
+%     rotated_image{k,["X_pos","Y_pos"]} = m.';
+% end
+
 rotated_image(:,"land||water") = T(:,"Var3");
 
 end
