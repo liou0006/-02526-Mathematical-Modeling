09ea5dd19aef138fcbe32b33a5e45113b588423a
squideddy <31780190+squideddy@users.noreply.github.com>
2025-02-25 13:40:21 +0100

    apply activation abs done

--- NeuralNetwork/apply_activation.m
+++ NeuralNetwork/apply_activation.m
@@ -1,3 +1,10 @@
-function output = apply_activation(input, activation = 'abs'):
+function output = apply_activation(input_vector, activation)
+output = [0;0];
+switch activation 
+    case 'abs'
+        output(1,1) = abs(input_vector(1));
+        output(2,1) = input_vector(2);
 
+    otherwise 
+        output = null
 end
