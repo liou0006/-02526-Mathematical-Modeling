efdbcc0f847f9db9a5c6c63c07c034bacf6d87ff
nielsT <niels.frederik.tabouillot@sentia.dk>
2025-03-04 12:33:21 +0100

    update main

--- NeuralNetwork/main.m
+++ NeuralNetwork/main.m
@@ -1,8 +1,7 @@
-
-T = readtable("Libian_desert_data.csv");
 close all
-rotatelist = [90,0,0,0]; % change rotation amount here
-biaslist = [180,-10,0,0];
+T = readtable("Libian_desert_data.csv");
+rotatelist = [90,-10,160,0]; % change rotation amount here
+biaslist = [180,0,450,0];
 for i=1:1:numel(biaslist)
     rotation=rotatelist(i);
     rotatedImage = rotate_image(T,rotation);
@@ -11,14 +10,12 @@ for i=1:1:numel(biaslist)
     biasedimage = apply_bias(rotatedImage,bias);  % applying bias
 
     foldedimage = apply_activation(biasedimage,'abs');
-    T=foldedimage;
+    T=biasedimage;
 
     x_values_bias = biasedimage(:,1);
     x_values_folded=foldedimage(:,1);
     % Find indices where a mirrored version exists
     gg = x_values_folded.Var1 - x_values_bias.Var1;
-    mirrored_indices = false(size(gg));
-
     m_index = find(gg>0);
     array_fold = foldedimage(m_index,:);
     % foldedimage(1,:) = -foldedimage(1,:);
@@ -27,25 +24,23 @@ for i=1:1:numel(biaslist)
     %
     % writematrix(foldedimage, 'transformed_data.csv');
     %
-    clf reset
-    figure(1)
+    figure(i)
     hold on
     title('Folded')
-
     scatter(foldedimage,"Var1","Var2","ColorVariable","Var3","SizeData",1,MarkerFaceAlpha=.1)
     scatter(array_fold,"Var1","Var2","ColorVariable","Var3","SizeData",1,MarkerFaceAlpha=.1)
     xline(0,'Color','red','LineWidth',1.5)
     hold off
-    figure(2)
+    figure(i+numel(biaslist))
     hold on
     title('OG')
     scatter(biasedimage,"Var1","Var2","ColorVariable","Var3","SizeData",1,MarkerFaceAlpha=.1)
     xline(0,'Color','red','LineWidth',1.5)
-    fig = figure();
+    hold off
+    fig = figure(i+numel(biaslist)*2);
     btn = uicontrol('Style', 'pushbutton', 'String', 'Continue', ...
         'Position', [20 20 100 40], ...
         'Callback', 'uiresume(gcbf)');
-
     uiwait(fig); % Waits for the button to be clicked
-    close(fig); % Closes the figure after clicking
+    close(fig);
 end
