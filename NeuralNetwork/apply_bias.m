fa227455c9c88e2c14575a04298d47d80ecf085c
Edward li <edwardtheli@gmail.com>
2025-03-14 14:19:33 +0100

    bias change

--- NeuralNetwork/apply_bias.m
+++ NeuralNetwork/apply_bias.m
@@ -1,7 +1,5 @@
-function [biased_table] = apply_bias(T,b)
+function [T] = apply_bias(T,b)
 % moves the input_vector by b along the x-axis 
-biased_table = T ; 
-% biased_table = biased_table + b;
-biased_table{:,"Var1"} = T{:,"Var1"} +b ;
-% biased_table(:,1) = T(:,1) +b;
+% biased_table = T ; 
+T{:,"Var1"} = T{:,"Var1"} +b ;
 end
