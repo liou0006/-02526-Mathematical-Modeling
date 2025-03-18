3832c787360a8396a1fa29801977c70d9eba3f63
Liou Xia <s224028@dtu.dk>
2025-03-18 11:33:24 +0100

    DELETED MAIN

--- Qatarra Depression/main.jl
+++
@@ -1,1 +0,0 @@
-readlines(raw"C:\Users\liou-\OneDrive - Danmarks Tekniske Universitet\C. Elektroteknologi - Bachelor\6. semester\02526 Mathematical Modeling\-02526-Mathematical-Modeling\Qatarra Depression\channel_data.txt")
