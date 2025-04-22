e08b6a7d3c24d7b010e7abfc601fc986c18fa8b4
Sjelle132 <117978670+Sjelle132@users.noreply.github.com>
2025-04-22 12:19:16 +0200

    Create fit.m
    suck it

---
+++ Exam project/folderOscar/fit.m
@@ -0,0 +1,61 @@
+
+function [Mdl, fitinfo] = fit(TrainFolder)
+
+if ~isfolder(TrainFolder)
+    errorMessage = sprintf('Error: The following folder does not exist:\n%s\nPlease specify a new folder.', TrainFolder);
+    uiwait(warndlg(errorMessage));
+    TrainFolder = uigetdir(); % Ask for a new one.
+    if TrainFolder == 0
+        % User clicked Cancel
+        return;
+    end
+end
+
+% Get all PNG files from the folder
+extracted_Trained = fullfile(TrainFolder, '*.png');
+theFiles = dir(extracted_Trained);
+
+
+% Check if there are any PNG files
+if isempty(theFiles)
+    error('No PNG files found in the selected folder.');
+end
+
+% Initialize a cell array to hold the images
+image_Array = cell(1, numel(theFiles));
+image_cells_filtered = cell(1,numel(theFiles));
+hasPnemoniaTrain = zeros(1,numel(theFiles))';
+
+kernel = fspecial("gaussian",4,.5);
+
+% Read each image
+for k = 1:numel(theFiles)
+    baseFileName = theFiles(k).name;
+    fullFileName = fullfile(theFiles(k).folder, baseFileName);
+    image_Array{k} = imread(fullFileName);
+    %image_cells_filtered{k} = imfilter(image_Array{k},kernel);
+    
+    [M, theta] = imgradient(image_Array{k});
+
+    image_cells_filtered{k} = M;
+%     
+%     figure('Name', 'mat2gray')
+%     imshow(mat2gray(M))
+   
+    if (contains(baseFileName,"positive"))
+        % fprintf(1, 'Now reading %s\n', fullFileName);
+        hasPnemoniaTrain(k) = 1;
+    else
+        hasPnemoniaTrain(k) = 0;
+    end
+end
+ALL_ARRAY = zeros(length(image_cells_filtered{1})^2,length(image_cells_filtered));
+% creates cells with 1-column vectors
+for k = 1:numel(image_cells_filtered) 
+    image_cells_filtered{k} = reshape(image_cells_filtered{k},[],1);
+    ALL_ARRAY(:,k) = image_cells_filtered{k} ;
+end
+
+[Mdl,fitinfo] = fitclinear(ALL_ARRAY', hasPnemoniaTrain , "ObservationsIn","rows","Regularization",'ridge', 'Lambda','auto')
+
+end
