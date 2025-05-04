90401c1bbc30fb9ad82b50276965d858ebf843e5
Edward li <edwardtheli@gmail.com>
2025-05-04 23:06:58 +0200

    fixed part 3 and part 1

--- Exam project/fitPart1.m
+++ Exam project/fitPart1.m
@@ -1,41 +1,47 @@
-function [Mdl, fitinfo] = fitPart1(TrainFolder)
+function [Mdl, fitinfo, ALL_ARRAY] = fitPart1(TrainFolder,lambda)
 
-filePatternTest = fullfile(TrainFolder, '*.png');
-theFiles = dir(filePatternTest);
 
-image_Array = cell(1, numel(theFiles));
-hasPnemoniaTrain = zeros(1,numel(theFiles))';
 
-% Read each image
-for k = 1:numel(theFiles)
-
-    baseFileName = theFiles(k).name;
-    fullFileName = fullfile(theFiles(k).folder, baseFileName);
-    image_Array{k} = imread(fullFileName);
-
-    if (contains(baseFileName,"positive"))
-        % fprintf(1, 'Now reading %s\n', fullFileName);
-        hasPnemoniaTrain(k) = 1;
-    else
-        hasPnemoniaTrain(k) = 0;
-    end
-
-end
-
-ALL_ARRAY = zeros(length(image_Array{1})^2,length(image_Array));
-
-% creates cells with 1-column vectors
-for k = 1:numel(image_Array)
-
-    image_Array{k} = reshape(image_Array{k},[],1);
-    ALL_ARRAY(:,k) = image_Array{k};
-
-end
+hasPnemoniaTrain = extract_posneg(TrainFolder);
+extracted_photos = extract_photos_from_folder(TrainFolder);
+ALL_ARRAY = extracted_photos2ALL_ARRAY(extracted_photos);
 
 [Mdl,fitinfo] = fitclinear(ALL_ARRAY', hasPnemoniaTrain , ...
     "ObservationsIn","rows", ...
     "Regularization",'ridge', ...
     "Learner","logistic",...
-    'Lambda','auto');
+    'Lambda', lambda);
 
+end
+
+
+% turns the Testfolder to a (elements in photo x num of photos matrix)
+function ALL_ARRAY = folder2ALL_ARRAY(TestFolder) 
+extracted_photos = extract_photos_from_folder(TestFolder);
+flattened = cellfun(@(img) (reshape(img, 1,[] )'), extracted_photos, UniformOutput=false);
+ALL_ARRAY = double(cat(2, flattened{:}));
+end
+
+% turns the extracted_photos to a (elements in photo x num of photos matrix)
+function ALL_ARRAY = extracted_photos2ALL_ARRAY(extracted_photos)
+flattened = cellfun(@(img) (reshape(img, 1,[] )'), extracted_photos, UniformOutput=false);
+ALL_ARRAY = double(cat(2, flattened{:}));
+end
+
+% extracted_photos takes the folder path and spits out the raw image data
+% returns a cell with number of photos by 1. 
+% each cell is the photos pixels
+function extracted_photos = extract_photos_from_folder(folderpath)
+Testing_photos = dir(fullfile(folderpath,'*.png'));
+Testing_photos_name = {Testing_photos.name}';
+test_pre_rawimage = fullfile({Testing_photos.folder}', Testing_photos_name);
+extracted_photos = cellfun(@imread, test_pre_rawimage, 'UniformOutput', false);
+end
+
+% extracted_photos takes the folder path and spits out a matrix of 1 or 0
+% matrix is number of photos by 1
+function extracted_posneg = extract_posneg(folderpath)
+Testing_photos = dir(fullfile(folderpath,'*.png'));
+Testing_photos_name = {Testing_photos.name}';
+extracted_posneg = contains(Testing_photos_name, "positive");
 end
--- Exam project/part1.m
+++ Exam project/part1.m
@@ -1,45 +1,33 @@
-
-
 %%
 % Specify the folder where the files live.
 TrainFolder = "C:\Users\edwar\OneDrive\Documents\TAMU files\spring 2025\mathmatical models\Final Exam\Train";
 TestFolder = "C:\Users\edwar\OneDrive\Documents\TAMU files\spring 2025\mathmatical models\Final Exam\Test";
 
-[Mdl, fitinfo] = fitPart1(TrainFolder);
-[Label, Score, hasPnemoniaTest] = modelPredictPart1(TestFolder, Mdl);
+lambda = linspace(0, 1, 200); % determine lambda
+[Mdl, fitinfo, ALL_ARRAY] = fitPart1(TrainFolder,lambda); % create the model
+[Label, Score, hasPnemoniaTest] = modelPredictPart1(TestFolder, Mdl); % test the model on test folder
 
-resultTable(:,1) = Label;
-resultTable(:,2) = hasPnemoniaTest;
-resultTable(:,4) = Score(:,1);
-resultTable(:,5) = Score(:,2);
+% create matrix to compare the prediction and actual result
+testing = zeros(size(Label,1), size(Label,2), 3);
+testing(:,:,1) = Label;
+testing(:,:,2) = repmat(hasPnemoniaTest, 1, size(Label,2));
+% Evaluate 
+testing(:,:,3) = testing(:,:,1) == testing(:,:,2);
 
-for k = 1:numel(hasPnemoniaTest)
-    if resultTable(k,1) == resultTable(k,2)
-        resultTable(k,3) = 1;
-    else
-        resultTable(k,3) = 0;
-    end
-end
-
-fprintf('How many positive images:')
-disp(sum(resultTable(:,3)));
-fprintf('How many percentages of total image is true:')
-disp((sum(resultTable(:,3))/numel(hasPnemoniaTest))*100);
-
-PartNumber = 1;
-writeToTable(resultTable, PartNumber);
+correct = sum(testing(:,:,3), 1); % determines how many the model got right
+percent = (correct) / size(testing,1); % what percent the model got right
 
 Beta = Mdl.Beta;
-betaim = reshape(Beta,224,224);
+betaim = reshape(Beta,224,224,length(lambda));
 betaim = abs(betaim);
 
 titleOfBeta = "Abs Value of learned weights image";
 
 h1 = figure;
-imshow(betaim, []);            
+imshow(betaim(:, :, 1), []);
 colormap(gca);          
 colorbar;
 title(titleOfBeta);
 saveas(gca,titleOfBeta,'png')
 
-clear filePatternTest filePatternTrain fullFileNameTest fullFileNameTrain baseFileNameTest baseFileNameTrain TestFolder TrainFolder
+% clear filePatternTest filePatternTrain fullFileNameTest fullFileNameTrain baseFileNameTest baseFileNameTrain TestFolder TrainFolder
